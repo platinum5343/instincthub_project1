7db95ba605318f8de9facdd3025b6d256862a39e
platinum5343 <olatunjiolamiji06@gmail.com>
2023-03-17 03:57:42 +1400

    new comit

---
+++ operator.js
@@ -0,0 +1,77 @@
+//  declare two let variables
+
+let num1 = 10;
+let num2 = 5;
+
+//  addition
+
+console.log('num1 + num2', num1 + num2);
+
+
+// subtraction
+
+console.log('num1 - num2', num1 - num2);
+
+
+// multiplication
+
+console.log('num1 * num2', num1 * num2);
+
+
+// division
+
+console.log('num1 / num2', num1 / num2);
+
+// remiander
+
+console.log('num1 % num2', num1 % num2);
+
+// incerment
+
+console.log('++num1', ++num1);
+console.log('num1++', num1++);
+console.log('++num2', ++num2);
+console.log('num2++', num2++);
+
+// decrement
+console.log('--num1', --num1);
+console.log('num1--', num1--);
+console.log('--num2', --num2);
+console.log('num2--', num2--);
+
+// using prompt() function
+
+
+let input1 = prompt();
+let input2 = prompt();
+// converting input to numbers using parseFloat
+
+ parseFloat(input1);
+ parseFloat(input2);
+
+
+// performing arithmetic operations  on the inputs
+
+//  addition
+
+console.log('input1 + input2', input1 + input2);
+
+
+// subtraction
+
+console.log('input1 - input2', input1 - input2);
+
+
+// multiplication
+
+
+console.log('input1 * input2', input1 * input2);
+
+// division
+
+console.log('input1 / input2', input1 / input2);
+
+// remiander
+
+console.log('input1 % input2', input1 % input2);
+
